a9535ff5782811c2575fccdd32b0a1cb9ea32c3e
조현승 <cho6646kr@hotmail.com>
2016-06-29 17:10:41 +0900

    phonebook updated

--- Common_Assignment_12/app/src/main/java/cs496/common_assignment_1/PhonebookSingleContactActivity.java
+++ Common_Assignment_12/app/src/main/java/cs496/common_assignment_1/PhonebookSingleContactActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class PhonebookSingleContactActivity extends Activity {
 
     @Override
@@ -17,13 +19,25 @@ public class PhonebookSingleContactActivity extends Activity {
         String name = in.getStringExtra("name");
         String email = in.getStringExtra("email");
         String mobile = in.getStringExtra("mobile");
+        String address = in.getStringExtra("address");
+        String gender = in.getStringExtra("gender");
+        String home = in.getStringExtra("home");
+        String office = in.getStringExtra("office");
 
         TextView txtName = (TextView) findViewById(R.id.txtname);
         TextView txtEmail = (TextView) findViewById(R.id.txtemail);
         TextView txtMobile = (TextView) findViewById(R.id.txtmobile);
+        TextView txtAddress = (TextView) findViewById(R.id.txtaddress);
+        TextView txtGender = (TextView) findViewById(R.id.txtgender);
+        TextView txtHome = (TextView) findViewById(R.id.txthome);
+        TextView txtOffice = (TextView) findViewById(R.id.txtoffice);
 
         txtName.setText(name);
         txtEmail.setText(email);
         txtMobile.setText(mobile);
+        txtAddress.setText(address);
+        txtGender.setText(gender);
+        txtHome.setText(home);
+        txtOffice.setText(office);
     }
 }
